d7b99a57c655b3473c343a412b8df508cc2ac422
Simon Martinelli <simon@72.services>
2019-05-02 10:32:54 +0200

    Initial version

--- awesome-ng/src/app/app.module.ts
+++ awesome-ng/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common';
 
 import { AppComponent } from './app.component';
 
